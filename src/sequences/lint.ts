f534b3ae8edd2ea63bfb56fcba454e4509325295
Kjell Dießel <development@kettil.de>
2020-08-23 23:52:32 +0200

    fix(lint): add option descriptions

--- src/sequences/lint.ts
+++ src/sequences/lint.ts
@@ -18,24 +18,24 @@ const options = { group: `${command}-Options` } as const;
 
 type Props = {
   ci: boolean;
-  run: typeof services[number] | undefined;
+  only: typeof services[number] | undefined;
 };
 
 export const builder: CommandModuleBuilder<Props> = builderDefault(command, (yargs) =>
   yargs.options({
-    ci: { ...options, type: 'boolean', default: false },
-    run: { ...options, alias: 'r', choices: services, implies: 'ci' },
+    ci: { ...options, type: 'boolean', default: false, describe: 'Run it in CI mode' },
+    only: { ...options, choices: services, implies: 'ci', describe: 'Run a single code quality tool' },
   }),
 );
 
 export const handler: CommandModuleHandler<Props> = async (argv) => {
   try {
     if (argv.ci) {
-      if (typeof argv.run === 'undefined' || argv.run === 'prettier') {
+      if (typeof argv.only === 'undefined' || argv.only === 'prettier') {
         await prettier(argv, { write: false, extension: extensionCi });
       }
 
-      if (typeof argv.run === 'undefined' || argv.run === 'eslint') {
+      if (typeof argv.only === 'undefined' || argv.only === 'eslint') {
         await eslint(argv, { write: false });
       }
     } else {
