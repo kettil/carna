367e9ec35a4a28a39d0ef7a8b57ef72a096203d0
Kjell Dießel <development@kettil.de>
2020-08-31 22:27:38 +0200

    refactor(config): update the babel config

--- templates/babel.config.js
+++ templates/babel.config.js
@@ -1,7 +1,10 @@
+const { env } = require('./node_modules/carna/configs/babel.config.js');
+
 module.exports = {
   extends: './node_modules/carna/configs/babel.config.js',
   presets: [
     ['@babel/env', { modules: '%BABEL_MODULE_TYPE%' }],
     // '@babel/preset-react',
   ],
+  env,
 };
