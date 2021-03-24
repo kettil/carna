b0a04dee96cbd522da49f8f97b75b61a370da0c3
Kjell Dießel <development@kettil.de>
2021-03-24 15:27:09 +0100

    fix: change javascript destination version to 12

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,5 @@ const { env } = require('./configs/babel.config.js');
 module.exports = {
   extends: './configs/babel.config.js',
   env,
-  presets: [['@babel/env', { modules: 'commonjs', targets: { node: '14' } }]],
+  presets: [['@babel/env', { modules: 'commonjs', targets: { node: '12' } }]],
 };
