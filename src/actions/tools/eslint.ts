0283e6f629900168565e2bd7c55f9d8702917a04
Kjell Dießel <development@kettil.de>
2021-03-24 10:27:07 +0100

    feat(actions): warnings are handled as errors in eslint

--- src/actions/tools/eslint.ts
+++ src/actions/tools/eslint.ts
@@ -16,7 +16,7 @@ const eslint: Action<Props> = async ({ cwd, cfg, log }, { write, files }) => {
   const hasConfigFile = await existConfigFile(cwd, configs);
 
   const cmd = './node_modules/.bin/eslint';
-  const args: string[] = ['--color'];
+  const args: string[] = ['--color', '--max-warnings', '0'];
 
   if (!hasConfigFile) {
     args.push('--config', relative(cwd, join(cfg, 'eslintrc.json')));
