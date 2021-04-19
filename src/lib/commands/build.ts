fd7326fcb077742bddba6f630c59fc379072fa05
Kjell Dießel <development@kettil.de>
2021-04-19 15:26:36 +0200

    docs: update command descriptions

--- src/lib/commands/build.ts
+++ src/lib/commands/build.ts
@@ -2,7 +2,7 @@ import { createBuilder, createHandler } from '../cli/yargs';
 import buildTask, { BuildProps } from '../tasks/buildTask';
 
 export const command = 'build';
-export const desc = 'Run the code quality tools';
+export const desc = 'Run the build process';
 
 export const handler = createHandler<BuildProps>(buildTask);
 export const builder = createBuilder<BuildProps>(command, (yargs) => yargs);
--- src/lib/commands/license.ts
+++ src/lib/commands/license.ts
@@ -2,7 +2,7 @@ import { createBuilder, createHandler } from '../cli/yargs';
 import licenseTask, { LicenseProps } from '../tasks/licenseTask';
 
 export const command = 'license';
-export const desc = 'Run the license check';
+export const desc = 'Run license check';
 
 export const handler = createHandler<LicenseProps>(licenseTask);
 export const builder = createBuilder<LicenseProps>(command, (yargs) => yargs);
