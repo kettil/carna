69625e1e9043dfe12b8845bf5839227c30315ddf
Kjell Dießel <development@kettil.de>
2021-04-19 16:13:01 +0200

    feat: add ci command

---
+++ src/lib/tasks/ciTask.ts
@@ -0,0 +1,19 @@
+import { PropsGlobal, Task } from '../types';
+import analyseTask from './analyseTask';
+import depsTask from './depsTask';
+import licenseTask from './licenseTask';
+import testTask from './testTask';
+
+export type CiProps = {};
+
+const ciTask: Task<CiProps> = async (argv) => {
+  const extendArgv: PropsGlobal = { ...argv, ci: true };
+
+  await analyseTask(extendArgv, {});
+  await testTask(extendArgv, { coverage: false });
+  await testTask(extendArgv, { coverage: true });
+  await licenseTask(extendArgv, {});
+  await depsTask(extendArgv, {});
+};
+
+export default ciTask;
