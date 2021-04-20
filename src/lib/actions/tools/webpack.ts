01034e7fd15fea931ca9721a7d66020d4c2f4e96
Kjell Dießel <development@kettil.de>
2021-04-20 23:41:44 +0200

    feat: run webpack only if the configuration file exists (build-command)

--- src/lib/actions/tools/webpack.ts
+++ src/lib/actions/tools/webpack.ts
@@ -2,13 +2,13 @@ import exec from '../../cmd/exec';
 import { existConfigFile } from '../../helper';
 import { Action } from '../../types';
 
-const configs = ['webpack.config.js'];
+export const webpackConfigList = ['webpack.config.js'];
 
 const webpack: Action = async ({ cwd, log }) => {
-  const hasConfigFile = await existConfigFile(cwd, configs);
+  const hasConfigFile = await existConfigFile(cwd, webpackConfigList);
 
   if (!hasConfigFile) {
-    throw new Error(`Webpack config file was not found (${configs.join(', ')})`);
+    throw new Error(`Webpack config file was not found (${webpackConfigList.join(', ')})`);
   }
 
   const cmd = './node_modules/.bin/webpack';
