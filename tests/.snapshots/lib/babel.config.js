3c8b26a7e5775359ac929f2350c7c22b4aeae0dd
Kjell Dießel <development@kettil.de>
2021-04-24 12:55:04 +0200

    fix: add @babel/plugin-transform-runtime for testing needs

--- tests/.snapshots/lib/babel.config.js
+++ tests/.snapshots/lib/babel.config.js
@@ -1,2 +1,4 @@
-// const { presets, env } = require('./configs/babel.config.js');
-module.exports = { extends: './configs/babel.config.js' };
+/* eslint-disable import/no-extraneous-dependencies */
+// const { presets, env } = require('carna/configs/babel.config.js');
+
+module.exports = { extends: './node_modules/carna/configs/babel.config.js' };
