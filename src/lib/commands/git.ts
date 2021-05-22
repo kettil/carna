e921d7c6657e40c96d16624f539e83a567aba7e3
Kjell Dießel <development@kettil.de>
2021-05-22 19:10:31 +0200

    feat: converting the git hook subcommand to a parameter
    BREAKING CHANGE
    The whole handling with the subcommand (git <hook>) turned out to be inflexible and therefore changed to a parameter.

--- src/lib/commands/git.ts
+++ src/lib/commands/git.ts
@@ -1,15 +1,16 @@
 import { createBuilder, createHandler } from '../cli/yargs';
 import gitTask, { GitProps } from '../tasks/gitTask';
 
-export const command = 'git <hook>';
+export const command = 'git';
 export const desc = 'Handler for the git hooks';
 
-const mode = ['msg', 'commit'] as const;
-const options = { group: `${command.slice(0, Math.max(0, command.indexOf('<'))).trim()}-Options` } as const;
+const mode: Array<GitProps['hook']> = ['msg', 'commit'];
+const options = { group: `${command}-Options` } as const;
 
 export const handler = createHandler<GitProps>(gitTask);
 export const builder = createBuilder<GitProps>(command, (yargs) =>
-  yargs.positional('hook', { choices: mode, demandOption: true }).options({
+  yargs.options({
+    hook: { ...options, choices: mode, demandOption: true },
     edit: { ...options, type: 'string', desc: 'Path to the COMMIT_EDITMSG file (only at hook "msg")' },
   }),
 );
