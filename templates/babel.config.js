f1127d61faa84ae57f16dc14c15e608388a8a969
Kjell Dießel <development@kettil.de>
2021-06-06 12:18:54 +0200

    fix: update eslint-disable comment in the template files

--- templates/babel.config.js
+++ templates/babel.config.js
@@ -1,4 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
+//* eslint-disable-next-line import/no-extraneous-dependencies -- is ok, because config file
 // const { presets, env } = require('carna/configs/babel.config.js');
 
 module.exports = { extends: './node_modules/carna/configs/babel.config.js' };
