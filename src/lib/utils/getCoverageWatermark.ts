0f3affd62118016fbb0afc0d45f8f9db6e02d6c2
Kjell Dießel <development@kettil.de>
2021-09-23 23:12:47 +0200

    feat: checks the order in coverage watermarks

--- src/lib/utils/getCoverageWatermark.ts
+++ src/lib/utils/getCoverageWatermark.ts
@@ -4,6 +4,10 @@ import { coverageDefaultWatermark } from '../../configs/actionConfigs';
 
 const getCoverageWatermark = (watermark?: Watermark | number | undefined): Watermark => {
   if (isArray(watermark)) {
+    if (watermark[0] > watermark[1]) {
+      throw new Error(`The medium-watermarks must not be greater than high-watermarks (${watermark})`);
+    }
+
     return watermark;
   }
 
