38525daf1eeccee724c88d2383a8622897bd27b5
Kjell Dießel <development@kettil.de>
2021-11-01 19:01:56 +0100

    fix: faulty argument combinations now throw an error

--- src/lib/commands/start.ts
+++ src/lib/commands/start.ts
@@ -5,7 +5,7 @@ const command = 'start';
 const desc = 'Run application directly';
 
 const options = { group: `${command}-Options` } as const;
-const boolOptions = { ...options, type: 'boolean', default: false } as const;
+const boolOptions = { ...options, type: 'boolean' } as const;
 
 const handler = createHandler<StartProps>(startTask);
 const builder = createBuilder<StartProps>(command, (yargs) =>
@@ -29,11 +29,14 @@ const builder = createBuilder<StartProps>(command, (yargs) =>
     'clear-console': {
       ...boolOptions,
       alias: 'c',
+      implies: ['watch'],
+      conflicts: ['pino-pretty'],
       describe: 'Clear console on each restart (only with watch mode)',
     },
     'pino-pretty': {
       ...boolOptions,
       alias: 'p',
+      conflicts: ['clear-console'],
       describe: 'Enables the pino log formatter (pino-pretty)',
     },
   }),
